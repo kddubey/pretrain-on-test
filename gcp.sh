3f6d11b108c5b52e71faa43cd8746c13207e5520
Kush Dubey <kushdubey63@gmail.com>
2024-05-29 00:30:10 -0700

    add perms and cpu install

--- gcp.sh
+++ gcp.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+# May need
+sudo apt update
+sudo apt install -y python3-pip git python3.11-venv
+
+
 # Set up venv
 python3 -m venv pretrain-env
 source pretrain-env/bin/activate
 git clone https://github.com/kddubey/pretrain-on-test.git
 cd pretrain-on-test
+# For CPU, don't install a bunch of nvidia packages:
+# python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 python -m pip install .
 
 
@@ -18,4 +25,4 @@ export PRETRAIN_ON_TEST_BUCKET_NAME="pretrain-on-test-accuracies"
 
 
 # Shutdown regardless of success or failure
-shutdown -h now
+sudo shutdown -h now
