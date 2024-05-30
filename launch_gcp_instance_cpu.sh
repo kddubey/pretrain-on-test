e4810489e15d1497992703842697512e43b12418
Kush Dubey <kushdubey63@gmail.com>
2024-05-29 23:26:38 -0700

    format INSTANCE_NAME

--- launch_gcp_instance_cpu.sh
+++ launch_gcp_instance_cpu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 PROJECT_NAME="virtual-equator-423819-v6"  # you'll change this
+INSTANCE_NAME="instance-pretrain-on-test-cpu-testing"
 
-gcloud beta compute instances create instance-pretrain-on-test-cpu-testing \
+gcloud beta compute instances create $INSTANCE_NAME \
     --project=$PROJECT_NAME \
     --zone=us-central1-a \
     --machine-type=e2-standard-2 \
@@ -13,11 +14,11 @@ gcloud beta compute instances create instance-pretrain-on-test-cpu-testing \
     --max-run-duration=7200s \
     --service-account=$SERVICE_ACCOUNT_EMAIL \
     --scopes=https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/trace.append,https://www.googleapis.com/auth/devstorage.read_write \
-    --create-disk=auto-delete=yes,boot=yes,device-name=instance-pretrain-on-test-cpu-testing,image=projects/debian-cloud/global/images/debian-12-bookworm-v20240515,mode=rw,size=80,type=projects/virtual-equator-423819-v6/zones/us-central1-a/diskTypes/pd-balanced \
+    --create-disk=auto-delete=yes,boot=yes,device-name=$INSTANCE_NAME,image=projects/debian-cloud/global/images/debian-12-bookworm-v20240515,mode=rw,size=80,type=projects/virtual-equator-423819-v6/zones/us-central1-a/diskTypes/pd-balanced \
     --no-shielded-secure-boot \
     --shielded-vtpm \
     --shielded-integrity-monitoring \
     --labels=goog-ec-src=vm_add-gcloud \
     --reservation-affinity=any
 
-gcloud compute ssh --zone "us-central1-a" "instance-pretrain-on-test-cpu-testing" --project "virtual-equator-423819-v6"
+gcloud compute ssh --zone "us-central1-a" $INSTANCE_NAME --project "virtual-equator-423819-v6"
