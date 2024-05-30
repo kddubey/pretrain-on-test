7b07edf906239ab20a1bc6e04d7191a069b44bf4
Kush Dubey <kushdubey63@gmail.com>
2024-05-29 23:10:23 -0700

    add max-run-duration

--- launch_gcp_instance_cpu.sh
+++ launch_gcp_instance_cpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PROJECT_NAME="virtual-equator-423819-v6"  # you'll change this
 
-gcloud compute instances create instance-pretrain-on-test-cpu-testing \
+gcloud beta compute instances create instance-pretrain-on-test-cpu-testing \
     --project=$PROJECT_NAME \
     --zone=us-central1-a \
     --machine-type=e2-standard-2 \
@@ -10,6 +10,7 @@ gcloud compute instances create instance-pretrain-on-test-cpu-testing \
     --maintenance-policy=TERMINATE \
     --provisioning-model=SPOT \
     --instance-termination-action=DELETE \
+    --max-run-duration=7200s \
     --service-account=$SERVICE_ACCOUNT_EMAIL \
     --scopes=https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/trace.append,https://www.googleapis.com/auth/devstorage.read_write \
     --create-disk=auto-delete=yes,boot=yes,device-name=instance-pretrain-on-test-cpu-testing,image=projects/debian-cloud/global/images/debian-12-bookworm-v20240515,mode=rw,size=80,type=projects/virtual-equator-423819-v6/zones/us-central1-a/diskTypes/pd-balanced \
@@ -19,7 +20,4 @@ gcloud compute instances create instance-pretrain-on-test-cpu-testing \
     --labels=goog-ec-src=vm_add-gcloud \
     --reservation-affinity=any
 
-gcloud compute ssh \
-    --zone "us-central1-a" \
-    "instance-pretrain-on-test-cpu-testing" \
-    --project $PROJECT_NAME
+gcloud compute ssh --zone "us-central1-a" "instance-pretrain-on-test-cpu-testing" --project "virtual-equator-423819-v6"
