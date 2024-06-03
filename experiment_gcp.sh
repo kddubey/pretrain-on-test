e11738407763e06f0937fae99c5dc3c7c142a912
Kush Dubey <kushdubey63@gmail.com>
2024-06-02 23:52:24 -0700

    yahoo_answers_topics

--- experiment_gcp.sh
+++ experiment_gcp.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
+
+# run-2024-06-03_05-48-48
+# python run.py \
+# --lm_type bert \
+# --dataset_names \
+#     yahoo_answers_topics \
+# --num_test 500 \
+# --num_subsamples 20 \
+# --per_device_train_batch_size_pretrain 8 \
+# --num_train_epochs_classification 1 \
+# --num_train_epochs_pretrain 2
+
+
 python run.py \
 --lm_type bert \
 --dataset_names \
-    yahoo_answers_topics \
+    SetFit/enron_spam \
+    yelp_review_full \
 --num_test 500 \
 --num_subsamples 20 \
 --per_device_train_batch_size_pretrain 8 \
+--per_device_train_batch_size_classification 4 \
+--per_device_eval_batch_size_classification 32 \
 --num_train_epochs_classification 1 \
 --num_train_epochs_pretrain 2
