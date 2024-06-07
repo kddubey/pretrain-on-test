61b41af8b89713096c28bda7e0582d25f1c6f5a2
Kush Dubey <kushdubey63@gmail.com>
2024-06-07 03:55:35 -0700

    tapify merge run script

--- analysis/dirty_file_processing/merge_runs.py
+++ analysis/dirty_file_processing/merge_runs.py
@@ -1,21 +1,35 @@
-"""
-Merge runs
-"""
-
 from pathlib import Path
 import shutil
 
-run_dir = "runs"
-destination_dir = Path("accuracies")
+from tap import tapify
 
-dataset_dirs: set[Path] = set()
-run_dirs = Path(run_dir).glob("*/")
-for run_dir in run_dirs:
-    for file in run_dir.rglob("*"):
-        if file.is_file() and file.parent != run_dir:
-            dataset_dirs.add(file.parent)
 
-destination_dir.mkdir()
-for dataset_dir in dataset_dirs:
-    dst = destination_dir / Path(*dataset_dir.parts[3:])
-    shutil.copytree(dataset_dir, dst, dirs_exist_ok=True)
+def merge(runs_dir: str, destination_dir: str):
+    """
+    Merge accuracy data collected across many runs into a single directory which can be
+    used for analysis.
+
+    Parameters
+    ----------
+    runs_dir : str
+        Directory containing `run-{timestamp}` directories, which contain the accuracy
+        data.
+    destination_dir : str
+        Directory which will contain all accuracy data from all runs.
+    """
+    dataset_dirs: set[Path] = set()
+    run_dirs = Path(runs_dir).glob("*/")
+    for run_dir in run_dirs:
+        for file in run_dir.rglob("*"):
+            if file.is_file() and file.parent != run_dir:
+                dataset_dirs.add(file.parent)
+
+    destination_dir_path = Path(destination_dir)
+    destination_dir_path.mkdir(exist_ok=True)
+    for dataset_dir in dataset_dirs:
+        dst = destination_dir_path / Path(*dataset_dir.parts[3:])
+        shutil.copytree(dataset_dir, dst, dirs_exist_ok=True)
+
+
+if __name__ == "__main__":
+    tapify(merge)
