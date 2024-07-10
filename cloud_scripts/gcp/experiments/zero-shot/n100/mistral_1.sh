048276b9fcc846c04b24f798636401d3e06b6ce4
Kush Dubey <kushdubey63@gmail.com>
2024-07-10 14:29:26 -0700

    double subsamples

--- cloud_scripts/gcp/experiments/zero-shot/n100/mistral_1.sh
+++ cloud_scripts/gcp/experiments/zero-shot/n100/mistral_1.sh
@@ -11,7 +11,7 @@ TQDM_DISABLE=1 python run.py \
         yelp_review_full \
         classla/FRENK-hate-en \
     --num_test 100 \
-    --num_subsamples 10 \
+    --num_subsamples 20 \
     --per_device_train_batch_size_pretrain 8 \
     --per_device_eval_batch_size_classification 8 \
     --num_train_epochs_pretrain 1
--- cloud_scripts/gcp/experiments/zero-shot/n100/mistral_2.sh
+++ cloud_scripts/gcp/experiments/zero-shot/n100/mistral_2.sh
@@ -22,7 +22,7 @@ TQDM_DISABLE=1 python run.py \
         tweets_hate_speech_detection \
         app_reviews \
     --num_test 100 \
-    --num_subsamples 10 \
+    --num_subsamples 20 \
     --per_device_train_batch_size_pretrain 16 \
     --per_device_eval_batch_size_classification 16 \
     --num_train_epochs_pretrain 1
