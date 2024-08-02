a465c57c909df7f69631988de36449decc44e5dc
Kush Dubey <kushdubey63@gmail.com>
2024-08-01 18:30:27 -0700

    deprecated image

--- cloud_scripts/gcp/launch.py
+++ cloud_scripts/gcp/launch.py
@@ -103,7 +103,7 @@ def create_instance_command_cpu(
         f"--network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default "
         f"--service-account={gcp_service_account_email} "
         f"--scopes=https://www.googleapis.com/auth/cloud-platform,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/trace.append,https://www.googleapis.com/auth/devstorage.read_write "
-        f"--create-disk=auto-delete=yes,boot=yes,device-name={instance_name},image=projects/debian-cloud/global/images/debian-12-bookworm-v20240515,mode=rw,size={boot_disk_size},type=projects/{project_name}/zones/{zone}/diskTypes/pd-balanced "
+        f"--create-disk=auto-delete=yes,boot=yes,device-name={instance_name},image=projects/debian-cloud/global/images/debian-12-bookworm-v20240611,mode=rw,size={boot_disk_size},type=projects/{project_name}/zones/{zone}/diskTypes/pd-balanced "
         f"--no-shielded-secure-boot "
         f"--shielded-vtpm "
         f"--shielded-integrity-monitoring "
