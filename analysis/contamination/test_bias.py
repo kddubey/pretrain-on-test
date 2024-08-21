32c06e3278e6206410966e607f3d7637841f2775
Kush Dubey <kushdubey63@gmail.com>
2024-08-21 12:34:33 -0700

    merge accuracies

--- analysis/contamination/test_bias.py
+++ analysis/contamination/test_bias.py
@@ -4,11 +4,8 @@ import numpy as np
 import polars as pl
 from scipy.stats import permutation_test
 
-paths = [
-    "run-2024-08-13_19-06-44-n500_contamination",
-    "run-2024-08-21_00-45-59-n500_contamination",
-]
 path_acc = os.path.join(
+    "run-2024-08-13_19-06-44-n500_contamination",
     "accuracies",
     "m100",
     "n500",
@@ -17,17 +14,27 @@ path_acc = os.path.join(
     "accuracies.csv",
 )
 
-df = pl.concat([pl.read_csv(os.path.join(path, path_acc)) for path in paths])
+df = pl.read_csv(path_acc)
+num_subsamples = len(df)
 
-print(df.describe())
+# There was a training run that super underperformed. Excluding it causes:
+# mean_eval_bias=0.0032, p_value=0.4057
+
+# acc_thresh = 0.43
+# mask = (pl.col("extra") > acc_thresh).and_((pl.col("test") > acc_thresh))
+# df = df.filter(mask)
+# assert len(df) == num_subsamples - 1
+
+# print(df.describe())
 
 p_value: float = permutation_test(
     data=(df["test"], df["extra"]),
     statistic=lambda x, y: np.mean(x - y),
     alternative="greater",  # acc_test > acc_extra
     permutation_type="samples",  # paired observations
-    n_resamples=10_000,
+    n_resamples=100_000,
 ).pvalue
 
 mean_eval_bias = df.select(pl.col("test") - pl.col("extra")).mean().item()
-print(f"{mean_eval_bias=}, {p_value=}")  # 0.25
+print(f"{mean_eval_bias=:.4f}, {p_value=:.4f}")
+# mean_eval_bias=0.0105, p_value=0.2409
